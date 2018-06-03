56f0307e9b708c633d54be412c3bb0b1d77eea37
David Levering <davidlevering@Davids-Air.fios-router.home>
2018-06-03 13:48:36 -0400

    start with pandas

--- iex_trading.py
+++ iex_trading.py
@@ -1,6 +1,9 @@
 import requests
 from pprint import pprint
 from utils import Utils
+import numpy as np
+import pandas as pd
+from pandas.io.json import json_normalize
 
 IEX_API_URL = "https://api.iextrading.com/1.0"
 IEX_BATCH_LIMIT = 100
@@ -22,6 +25,7 @@ class IEXTrading:
     def refresh(self, symbols):
         batches = list(Utils.chunks(symbols, 100))
         base = "/stock/market/batch?"
+        print("Gathering market data...")
         for chunk in batches:
             #set up the parameters for our API request
             params = dict(
@@ -29,5 +33,7 @@ class IEXTrading:
                 types = "price,ohlc,stats,quote"
             )
             response = requests.get(url=IEX_API_URL + base, params=params)
-            data = response.json()
-            pprint(data)
+            json = response.json()
+            df = pd.DataFrame({col: dict(vals) for col, vals in json.items()})
+            print(df)
+        print("Done!")
