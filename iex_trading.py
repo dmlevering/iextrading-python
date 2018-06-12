f11c7375082aac314e336802a6b298d86ebcf493
David Levering <dmlevering@gmail.com>
2018-06-11 21:57:33 -0400

    progress towards consolidating data

--- iex_trading.py
+++ iex_trading.py
@@ -8,59 +8,75 @@ from datetime import datetime, timedelta
 import sys
 import csv
 from bs4 import BeautifulSoup
+from enum import Enum
 
 class IEXTrading(object):
     #paths and filenames
-    PATH_CACHE = "cache/"
-    PATH_SYMBOLS = "symbols/"
-    FILENAME_METADATA = "meta.txt"
+    PATH_CACHE           = "cache/"
+    PATH_SYMBOLS         = "symbols/"
+    FILENAME_METADATA    = "meta.txt"
     FILENAME_MARKET_DATA = "market_data.csv"
-    FILENAME_NASDAQ = "nasdaq.csv"
-    FILENAME_NYSE = "nyse.csv"
-    FILENAME_AMEX = "amex.csv"
+    FILENAME_NASDAQ      = "nasdaq.csv"
+    FILENAME_NYSE        = "nyse.csv"
+    FILENAME_AMEX        = "amex.csv"
+
+    #enums
+    class DataType(Enum):
+        BATCH            = "batch"
+        BOOK             = "book"
+        CHART            = "chart"
+        COMPANY          = "company"
+        DELAYED_QUOTE    = "delayed-quote"
+        DIVIDENDS        = "dividends"
+        EARNINGS         = "earnings"
+        EFFECTIVE_SPREAD = "effective-spread"
+        FINANCIALS       = "financials"
+        GAINERS          = "gainers"
+        KEY_STATS        = "stats"
+        LARGEST_TRADES   = "largest-trades"
+        LIST             = "list"
+        LOGO             = "logo"
+        LOSERS           = "losers"
+        MARKET           = "market"
+        MOST_ACTIVE      = "mostactive"
+        NEWS             = "news"
+        PRICE            = "price"
+        QUOTE            = "quote"
+        STOCK            = "stock"
+        TIME_SERIES      = "time-series"
+
+    class DataRange(Enum):
+        FIVE_YEARS   = "5y"      #historically adjusted market-wide data
+        TWO_YEARS    = "2y"      #^
+        YEAR_TO_DATE = "ytd"     #^
+        SIX_MONTHS   = "6m"      #^
+        THREE_MONTHS = "3m"      #^
+        ONE_MONTH    = "1m"      #^
+        ONE_DAY      = "1d"      #IEX-only data by minute
+        DATE         = "date"    #IEX-only data by minute for a specified date
+                                 #in the format YYYYMMDD (e.g., /date/19970408)
+        DYNAMIC      = "dynamic" #returns 1d or 1m data depending on the day
+                                 #or week and time of day. Intraday per minute
+                                 #data is only returned during market hours.
 
     #constants
     API_URL_IEX = "https://api.iextrading.com/1.0"
     BATCH_LIMIT_IEX = 100
     MAX_MARKET_DATA_AGE_MINUTES = 120
     DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
-    TIME_SERIES_RANGE = ["5y",   #historically adjusted market-wide data
-                         "2y",   #^
-                         "ytd",  #^
-                         "6m",   #^
-                         "3m",   #^
-                         "1m",   #^
-                         "1d",   #IEX-only data by minute
-                         "date", #IEX-only data by minute for a specified date
-                                 #in the format YYYYMMDD (e.g., /date/19970408)
-                         "dynamic", #returns 1d or 1m data depending on the day
-                                    #or week and time of day. Intraday per minute
-                                    #data is only returned during market hours.
+    MARKET_DATA_TYPES = [DataType.QUOTE.value,
+                         DataType.EARNINGS.value,
+                         DataType.FINANCIALS.value,
+                         DataType.KEY_STATS.value,
+                         DataType.COMPANY.value
                         ]
 
     def __init__(self):
         self.symbols = self._read_symbols()
         self.market_data = self._read_market_data()
         self.sp500 = self.get_sp500()
-        print(self.best_peratio())
-        symbols = ["AAPL","MSFT", "GOOG", "AMZN"]
-        dfs = self._api_request_time_series(symbols, "2y")
-        #for df in dfs:
-        #    df["date"] = pd.to_datetime(df["date"])
-        #    df.plot(x="date", y="close")
-        #df_apple_ts["date"] =  pd.to_datetime(df_apple_ts["date"])
-        #df_apple_ts.plot(x="date", y="close")
-        #plt.show()
+        #print(self.best_peratio())
 
-        fig, ax = plt.subplots()
-        for i, df in enumerate(dfs):
-            df["date"] = pd.to_datetime(df["date"])
-            df.plot(ax=ax, x="date", y="close", label=symbols[i])
-
-        plt.ylabel("Share price (dollars)")
-        plt.xlabel("Date")
-        plt.title("Share Price over Time")
-        plt.show()
 
         #stats
         df = self.stats(symbols)
@@ -85,7 +101,7 @@ class IEXTrading(object):
         self.market_data = _api_request_market_data()
 
     def company_info(self, symbol):
-        http_req = "/stock/" + symbol + "/company"
+        http_req = "/" + self.DataType.STOCK.value + "/" + symbol + "/" + self.DataType.COMPANY.value
         response = requests.get(self.API_URL_IEX + http_req)
         json = response.json()
         df = json_normalize(json)
@@ -130,7 +146,8 @@ class IEXTrading(object):
     def _api_request_time_series(self, symbols, range):
         dfs = []
         for symbol in symbols:
-            request_base = "/stock/" + symbol + "/chart/" + range
+            request_base = ("/" + self.DataType.STOCK.value + "/" + symbol + "/" +
+                            self.DataType.CHART.value + "/" + range)
             response = requests.get(url=self.API_URL_IEX + request_base)
             series_list = [pd.Series(day) for day in response.json()]
             df = pd.concat(series_list, axis=1).transpose()
@@ -139,25 +156,41 @@ class IEXTrading(object):
 
     def _api_request_market_data(self):
         symbol_batches = list(Utils.chunks(self.symbols, self.BATCH_LIMIT_IEX))
-        request_base = "/stock/market/batch?"
+        request_base = ("/" + self.DataType.STOCK.value + "/" + self.DataType.MARKET.value + "/" +
+                        self.DataType.BATCH.value + "?")
         series_list = []
         print("Requesting market data through API...")
         for batch in symbol_batches:
             #set up the parameters for API request
             params = dict(
                 symbols = ','.join(batch),
-                types = "quote"
+                types = ",".join(self.MARKET_DATA_TYPES)
             )
-            response = requests.get(url=self.API_URL_IEX + request_base, params=params)
-            for k,v in response.json().items():
-                for qk, val in v.items():
-                    ser = pd.Series(val)
-                    series_list.append(ser)
-        df = pd.concat(series_list, axis=1).transpose()
-        df.set_index("symbol", inplace=True)
-        print("Done!")
-        self._save_market_data(df)
-        return df
+            response_json = requests.get(url=self.API_URL_IEX + request_base, params=params).json()
+            #results = {data_type : response_json[data_type] for data_type in self.MARKET_DATA_TYPES}
+            for symbol, data in response_json.items():
+                self._parse_quote(data)
+
+            break
+
+            #for k,v in response.json().items():
+                #for qk, val in v.items():
+                    #ser = pd.Series(val)
+                    #series_list.append(ser)
+        #df = pd.concat(series_list, axis=1).transpose()
+        #df.set_index("symbol", inplace=True)
+        #print("Done!")
+        #self._save_market_data(df)
+        #return df
+
+    def _parse_quote(self, data):
+        pprint(data)
+        quote = data[self.DataType.QUOTE.value]
+        if quote is None:
+            print("wtf")
+            return None
+        pprint(quote)
+
 
     def get_sp500(self):
         #grab a list of the current S&P 500 symbols (tickers) from Wikipedia
@@ -189,8 +222,25 @@ class IEXTrading(object):
         #7: Is Days to Cover less than 2?
         #8: Is the net activity positive (more buyers than sellers) over the last 3 months?
 
-    def plot(dfs, xcol, ycol):
-        pass
+    def plot():
+        symbols = ["GOOG", "AMZN"]
+        dfs = self._api_request_time_series(symbols, "ytd")
+        #for df in dfs:
+        #    df["date"] = pd.to_datetime(df["date"])
+        #    df.plot(x="date", y="close")
+        #df_apple_ts["date"] =  pd.to_datetime(df_apple_ts["date"])
+        #df_apple_ts.plot(x="date", y="close")
+        #plt.show()
+
+        fig, ax = plt.subplots()
+        for i, df in enumerate(dfs):
+            df["date"] = pd.to_datetime(df["date"])
+            df.plot(ax=ax, x="date", y="close", label=symbols[i])
+
+        plt.ylabel("Share price (dollars)")
+        plt.xlabel("Date")
+        plt.title("Share Price over Time")
+        plt.show()
 
     def stats(self, symbols):
         series_list = []
